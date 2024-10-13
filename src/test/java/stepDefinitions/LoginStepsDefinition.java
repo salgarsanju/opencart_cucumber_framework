c72e80c32a2d62c00578491fd5d7c96a0dc09013
salgarsanju@gmail.com <salgarsanju@gmail.com>
2024-10-13 08:52:36 +0530

    My first commit

---
+++ src/test/java/stepDefinitions/LoginStepsDefinition.java
@@ -0,0 +1,50 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepsDefinition {
+	@Given("User Launch browser")
+	public void user_launch_browser() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("opens URL {string}")
+	public void opens_url(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User navigate to MyAccount menu")
+	public void user_navigate_to_my_account_menu() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("click on Login")
+	public void click_on_login() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User enters Email as {string} and Password as {string}")
+	public void user_enters_email_as_and_password_as(String string, String string2) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Click on Login button")
+	public void click_on_login_button() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User navigates to MyAccount Page")
+	public void user_navigates_to_my_account_page() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+}
